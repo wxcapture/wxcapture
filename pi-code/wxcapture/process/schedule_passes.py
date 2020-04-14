066c3d6d37fa2d38c170b29ec104bc960ac2f5b0
technobird22 <51074275+kiwibird22@users.noreply.github.com>
2020-04-14 21:38:51 +1200

    Fixed time formatting in the dynamic page generating code

--- pi-code/wxcapture/process/schedule_passes.py
+++ pi-code/wxcapture/process/schedule_passes.py
@@ -787,11 +787,11 @@ try:
 
         MY_LOGGER.debug('Footer')
         html.write('<footer class=\"main-footer\">')
-        html.write('<p id=\"footer-text\">Pass Dat last updated at <time>' +
+        html.write('<p id=\"footer-text\">Pass Dat last updated at <span class="time">' +
                    time.strftime('%H:%M (' +
                                  subprocess.check_output("date").
                                  decode('utf-8').split(' ')[-2] +
-                                 ')</time> on the <time>%d/%m/%Y</time>') +
+                                 ')</span> on the <span class="time">%d/%m/%Y</span>') +
                    '.</p>')
         html.write('</footer>')
         html.write('<script src=\"lightbox/js/lightbox-plus-jquery.min.js\"></script>')
--- server-code/wxcapture/web/move.py
+++ server-code/wxcapture/web/move.py
@@ -194,11 +194,11 @@ def build_month_page(bpm_file_path, bpm_file_name, bpm_month, bpm_month_name, bp
                              bpm_month_name + ' ' + bpm_year)
         cp_html.write('</section>')
         cp_html.write('<footer class=\"main-footer\">')
-        cp_html.write('<p id=\"footer-text\">Captures last updated at <time>' +
+        cp_html.write('<p id=\"footer-text\">Captures last updated at <span class="time">' +
                       time.strftime('%H:%M (' +
                                     subprocess.check_output("date").
                                     decode('utf-8').split(' ')[-2] +
-                                    ')</time> on the <time>%d/%m/%Y</time>') +
+                                    ')</span> on the <span class="time">%d/%m/%Y</span>') +
                       '.</p>')
         cp_html.write('</footer>')
         cp_html.write('</body></html>')

363c64a4ccc846ce0389720c19d9a7e31a50be86
KiwiInNZ <51867737+KiwiInNZ@users.noreply.github.com>
2020-04-19 11:17:19 +1200

    twitter test code

---
+++ pi-code/wxcapture/process/twitter_test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""twitter tweet test"""
+
+
+# import libraries
+import os
+
+from datetime import datetime
+import wxcutils
+import wxcutils_pi
+
+
+# setup paths to directories
+HOME = os.environ['HOME']
+APP_PATH = HOME + '/wxcapture/'
+CODE_PATH = APP_PATH + 'process/'
+LOG_PATH = CODE_PATH + 'logs/'
+OUTPUT_PATH = APP_PATH + 'output/'
+IMAGE_PATH = OUTPUT_PATH + 'images/'
+WORKING_PATH = CODE_PATH + 'working/'
+CONFIG_PATH = CODE_PATH + 'config/'
+
+# start logging
+MODULE = 'twitter_test'
+MY_LOGGER = wxcutils.get_logger(MODULE, LOG_PATH, MODULE + '.log')
+MY_LOGGER.debug('-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+')
+MY_LOGGER.debug('Execution start')
+MY_LOGGER.debug('APP_PATH = %s', APP_PATH)
+MY_LOGGER.debug('CODE_PATH = %s', CODE_PATH)
+MY_LOGGER.debug('LOG_PATH = %s', LOG_PATH)
+MY_LOGGER.debug('OUTPUT_PATH = %s', OUTPUT_PATH)
+MY_LOGGER.debug('IMAGE_PATH = %s', IMAGE_PATH)
+MY_LOGGER.debug('WORKING_PATH = %s', WORKING_PATH)
+MY_LOGGER.debug('CONFIG_PATH = %s', CONFIG_PATH)
+
+
+
+wxcutils_pi.tweet_text(CONFIG_PATH, 'config-twitter.json',
+                      'hello world (original I know) ' + datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+
+wxcutils_pi.tweet_text_image(CONFIG_PATH, 'config-twitter.json',
+                             'hello world with image (original I know) ' + datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+                             IMAGE_PATH + '2020-04-18-20-11-28-NOAA_18-norm-tn.jpg')
+
+MY_LOGGER.debug('Execution end')
+MY_LOGGER.debug('-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+')

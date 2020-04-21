d354c1b3121b8b1fe4a96ab949f3ff4b76199714
KiwiInNZ <51867737+KiwiInNZ@users.noreply.github.com>
2020-04-21 14:17:19 +1200

    added fixing a single file via cmd line args

--- server-code/wxcapture/web/fix_pass_pages.py
+++ server-code/wxcapture/web/fix_pass_pages.py
@@ -6,6 +6,7 @@
 import os
 from os import listdir
 from os.path import isfile, join
+import sys
 import fnmatch
 import wxcutils
 import fix_pass_pages_lib
@@ -32,16 +33,29 @@ MY_PATH = '/home/mike/wxcapture/output/'
 TARGET = '/media/storage/html/wxcapture/'
 OUTPUT_PATH = '/media/storage/html'
 
+# see if args passed
+try:
+    DIRECTORY = sys.argv[1]
+    FILE = sys.argv[2]
+except:
+    DIRECTORY = ''
+    FILE = ''
+
 MY_LOGGER.debug('Starting file fixing')
 
 # find the files to skip, changing only those for satellite passes
-for filename in fix_pass_pages_lib.find_files(TARGET, '*.html'):
-    if 'NOAA' in filename or '.backup' in filename or 'METEOR' in filename or 'ISS' in filename or 'SAUDISAT' in filename:
-        path_part, file_part = os.path.split(filename)
-        MY_LOGGER.debug('Fixing - filename = %s, path = %s, file = %s', filename, path_part, file_part)
-        fix_pass_pages_lib.fix_file(path_part + '/', file_part)
-    else:
-        MY_LOGGER.debug('SKIPing            = %s', filename)
+if DIRECTORY != '':
+    MY_LOGGER.debug('Fixing %s %s', DIRECTORY, FILE)
+    fix_pass_pages_lib.fix_file(DIRECTORY + '/', FILE)
+else:
+    MY_LOGGER.debug('Fixing ALL')
+    for filename in fix_pass_pages_lib.find_files(TARGET, '*.html'):
+        if 'NOAA' in filename or '.backup' in filename or 'METEOR' in filename or 'ISS' in filename or 'SAUDISAT' in filename:
+            path_part, file_part = os.path.split(filename)
+            MY_LOGGER.debug('Fixing - filename = %s, path = %s, file = %s', filename, path_part, file_part)
+            fix_pass_pages_lib.fix_file(path_part + '/', file_part)
+        else:
+            MY_LOGGER.debug('SKIPing            = %s', filename)
 
 
 MY_LOGGER.debug('Finished file fixing')

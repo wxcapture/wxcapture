8044c3a3a5f7b88540bb07b8219cc8070261d55f
technobird22 <51074275+kiwibird22@users.noreply.github.com>
2020-06-26 13:08:19 +1200

    Bugfixes

--- pi-code/wxcapture/process/equalise.py
+++ pi-code/wxcapture/process/equalise.py
@@ -6,7 +6,7 @@ import cv2
 def clahe(in_img):
     return cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4,4)).apply(in_img)
 
-def do_clahe_img(inp):
+def do_clahe_img(img):
     b_chn, g_chn, r_chn = cv2.split(img)
     return cv2.merge((clahe(b_chn), clahe(g_chn), clahe(r_chn)))
 
@@ -17,7 +17,7 @@ output = "clahe_"
 for img_path in inputs:
     print("Performing CLAHE on: \"" + img_path + "\"")
 
-    img = do_clahe_img(cv2.imread(img_path))
+    out_img = do_clahe_img(cv2.imread(img_path))
 
     cv2.imwrite(output + img_path, out_img)
 

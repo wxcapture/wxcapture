78eb46b341f3945f4d78d974878303e631f117d5
technobird22 <51074275+kiwibird22@users.noreply.github.com>
2020-06-26 12:59:42 +1200

    Simplified equalise.py
    Removed old unused code

--- pi-code/wxcapture/process/equalise.py
+++ pi-code/wxcapture/process/equalise.py
@@ -3,17 +3,10 @@ import sys
 import numpy as np
 import cv2
 
-
 def do_clahe(inp):
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4,4)).apply(inp)
     return clahe
 
-# if(len(sys.argv) == 1):
-#     print("Error: No input files")
-#     exit()
-
-# inputs = sys.argv[1:]
-# inputs = os.listdir()
 inputs = [""]
 
 output = "clahe_"
@@ -28,9 +21,7 @@ for img_path in inputs:
 
     b_chn, g_chn, r_chn = cv2.split(img)
 
-    b_chn, g_chn, r_chn = do_clahe(b_chn), do_clahe(g_chn), do_clahe(r_chn)
-
-    out_img = cv2.merge((b_chn, g_chn, r_chn))
+    out_img = cv2.merge((do_clahe(b_chn), do_clahe(g_chn), do_clahe(r_chn)))
 
     cv2.imwrite(output + img_path, out_img)
 

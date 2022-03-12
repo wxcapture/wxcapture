7b901650c5a8fd539b30252979cef21848051e04
KiwiInNZ <51867737+KiwiInNZ@users.noreply.github.com>
2022-03-12 18:00:17 +1300

    rsync debug

--- wsl/wxcapture/web/rs.sh
+++ wsl/wxcapture/web/rs.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+echo "rsync with ionice"
+echo $1 $2 $3 $4 $5
+
 if [[ "$2" == "--exclude" ]]
 then
+    echo "--exclude"
     /usr/bin/rsync $1 --rsync-path "ionice -c 3 nice rsync" $2 $3 $4 $5
 else
+    echo "non--exclude"
     /usr/bin/rsync $1 --rsync-path "ionice -c 3 nice rsync" $2 $3
 fi
